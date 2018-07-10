67630ee060dfe2494ca96e16001e260c60cdc18c
Tom Christopoulos <tom.christopoulos@platform9.com>
2018-07-10 12:22:30 -0400

    test

--- post-commit.sh
+++ post-commit.sh
@@ -4,11 +4,11 @@ echo "Post-Commit Githook Running"
 echo Running $BASH_SOURCE
 
 ENV_DATA=`env | grep GIT`
-#PWD="echo "PWD is: $(pwd)"
 PAYLOAD="{\"username\": \"webhookbot\", \"text\": \"This is post from $(whoami)@$(hostname) via a webhookbot. Testing a git hook, \`\`\` 
 User:$(whoami) 
 Host: $(hostname) 
 $ENV_DATA
+PWD is: $(pwd)
 \`\`\`\", \"icon_emoji\": \":ghost:\"}"
 
 curl -X POST --data-urlencode "payload=$PAYLOAD" https://hooks.slack.com/services/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
