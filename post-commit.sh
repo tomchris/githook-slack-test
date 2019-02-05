6468902574ed2a51fe434639af606916788a0144
Tom Christopoulos <tom.christopoulos@platform9.com>
2019-02-04 23:31:15 -0500

    Updated post-commit.sh
    Pulling in whole commit message (using --pretty=short)
    Added string manipulation to commit message to replace carriage returns
    with newline and escape all double quotes.
    Added branch.
    Changed Slack message format to use an attachment for the commit message.
    This will allow code blocks in the commit message to be displayed in
    slack if the format matches slack's formating.

--- post-commit.sh
+++ post-commit.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 BRANCH_NAME="$(git rev-parse --abbrev-ref HEAD)"
-COMMIT_MSG="$(git log -1 HEAD --pretty=format:%s)"
+COMMIT_MSG="$(git log -1 HEAD --pretty=medium |  awk -v ORS='\\n' '1' | awk '{gsub(/\"/,"\\\"")}8')"
 WEBHOOK_URL=$(cat ./webhook)
-PAYLOAD="{\"username\": \"webhookbot\", \"text\": \"GIT COMMIT from $(whoami)@$(hostname). 
-\`\`\` 
+PAYLOAD='{
+    "username":"webhookbot",
+    "icon_emoji":":ghost:",
+    "attachments":[
+    {
+      "fallback":"*GIT COMMIT* from '$(whoami)@$(hostname)'",
+      "color":"#36a64f",
+      "pretext":"*GIT COMMIT*",
+      "author_name":"'$(whoami)@$(hostname)'",
+      "text":"Branch: '$BRANCH_NAME'\n Commit Message: '$COMMIT_MSG'",
+      "ts":123456789
+    }
+    ]
+  }'
 
-User:$(whoami) 
-Host: $(hostname) 
-Branch: $BRANCH_NAME
-Commit Message: $COMMIT_MSG
-
-\`\`\`\", \"icon_emoji\": \":ghost:\"}"
-
-curl -X POST --data-urlencode "payload=$PAYLOAD" $WEBHOOK_URL 
+curl -v -X POST  --data-urlencode "payload=$PAYLOAD" -s $WEBHOOK_URL 
