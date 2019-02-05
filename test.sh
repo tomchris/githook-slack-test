14d9883d4a8bb42dd7bc44321063f476b6aec9a7
Tom Christopoulos <tom.christopoulos@platform9.com>
2019-02-04 20:10:35 -0500

    testing correct commit message variable

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@
 
 PAYLOAD="{\"username\": \"webhookbot\", \"text\": \"This is test post via a webhookbot. Jenkins\", \"icon_emoji\": \":ghost:\"}"
 
-curl -X POST --data-urlencode "payload=$PAYLOAD" ***REMOVED***
+curl -X POST --data-urlencode "payload=$PAYLOAD" https://hooks.slack.com/services/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
