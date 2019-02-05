23c14a71f13b5e6913391644ee27b249717298b4
Tom Christopoulos <tom.christopoulos@platform9.com>
2019-02-04 20:57:33 -0500

    updating test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+WEBHOOK_URL=$(cat ./webhook)
 PAYLOAD="{\"username\": \"webhookbot\", \"text\": \"This is test post via a webhookbot. Jenkins\", \"icon_emoji\": \":ghost:\"}"
 
-curl -X POST --data-urlencode "payload=$PAYLOAD" https://hooks.slack.com/services/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
+curl -X POST --data-urlencode "payload=$PAYLOAD" $WEBHOOK_URL 
