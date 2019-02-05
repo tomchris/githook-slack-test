3b89384f501f8810e99acecfcb15a6d17bf1d2be
Tom Christopoulos <tom.christopoulos@platform9.com>
2019-02-04 19:51:44 -0500

    Testing message format

--- post-commit.sh
+++ post-commit.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+BRANCH_NAME="$(git rev-parse --abbrev-ref HEAD)"
+COMMIT_MSG="$(cat $1)"
 WEBHOOK_URL=$(cat ./webhook)
-PAYLOAD="{\"username\": \"webhookbot\", \"text\": \"This is post from $(whoami)@$(hostname) via a webhookbot. Testing a git hook, 
+PAYLOAD="{\"username\": \"webhookbot\", \"text\": \"GIT COMMIT from $(whoami)@$(hostname). 
 \`\`\` 
+
 User:$(whoami) 
 Host: $(hostname) 
-$(env)
+Branch: $BRANCH_NAME
+Commit Message: $COMMIT_MSG
+
 \`\`\`\", \"icon_emoji\": \":ghost:\"}"
 
 curl -X POST --data-urlencode "payload=$PAYLOAD" $WEBHOOK_URL 
